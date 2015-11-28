179548dbb32b764b00e72c2ec00c1d4e96105f54
Thiago Aguiar <thiago.aguiar86@gmail.com>
2015-11-28 16:04:46 -0200

    Adicionando a estrutura funcional das features de destaque, junto com o plugin swiper adaptado, de acordo com o layout; Ajustes de css;

--- js/common.js
+++ js/common.js
@@ -4,3 +4,23 @@
  * @author Thiago Aguiar - Tuneduc
  * @copyright 2015
  ==================== JS FUNCTIONS ==================== */
+
+var swiper = new Swiper('.swiper-container', {
+	spaceBetween: 20,
+	autoplay: 3000,
+	hashnav: true,
+	keyboardControl: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+});
+
+var _changeFeature = {
+	init: function() {
+		var _thumbnail = $('a[data-slide]');
+		_thumbnail.on({
+			click: function() {
+				swiper.slideTo($(this).data('slide'));
+			}
+		});
+	}
+}
